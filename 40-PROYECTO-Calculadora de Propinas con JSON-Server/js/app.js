7ae2f616e68af89cac6337f1a21c728af3c743c2
DanielGesto <danielcristophervazquezayala@gmail.com>
2021-11-12 18:12:32 -0600

    first commit

---
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -0,0 +1,117 @@
+let cliente = {
+    mesa: '',
+    hora: '',
+    pedido: []
+};
+
+const categorias = {
+    1: 'Comida',
+    2: 'Bebidas',
+    3: 'Postres'
+}
+
+const btnGuardar = document.querySelector('#guardar-cliente');
+btnGuardar.addEventListener('click', guardarCliente);
+
+
+
+//FUNCIONES 
+
+function guardarCliente() {
+    const mesa = document.querySelector('#mesa').value;
+    const hora = document.querySelector('#hora').value;
+
+    //Revisar si hay campos vacios 
+    const camposVacios = [mesa, hora].some(campo => campo === '');
+    if(camposVacios) {
+
+        //Verificar si ya hay una alerta
+        const existeAlerta = document.querySelector('.invalid-feedback');
+
+        if(!existeAlerta) {
+            const alerta = document.createElement('DIV');
+            alerta.classList.add('invalid-feedback', 'd-block', 'text-center');
+            alerta.textContent = 'Todos los campos son obligatorios';
+            document.querySelector('.modal-body form').appendChild(alerta);
+            //Elimina la alerta
+            setTimeout(() => {
+                alerta.remove();
+            }, 3000);
+        }
+        return;       
+    } 
+    //Asignar datos del formulario
+    cliente = {...cliente, mesa, hora}
+
+    //Ocultar modal 
+    const modalFormulario = document.querySelector('#formulario');
+    const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+    modalBootstrap.hide();
+
+    //Mostrar las secciones
+    mostrarSecciones();
+
+    //Obtener Platilos de la API de JSON-SERVER
+    obtenerPlatillos();
+}
+
+function mostrarSecciones() {
+    const seccionesOcultas = document.querySelectorAll('.d-none');
+    seccionesOcultas.forEach(seccion => seccion.classList.remove('d-none'))
+}
+
+function obtenerPlatillos() {
+    const url = 'http://localhost:4000/platillos'
+    fetch(url)
+        .then(response => response.json())
+        .then(result => mostrarPlatillos(result))
+        .catch(error => console.log(error))
+}
+
+function mostrarPlatillos(platillos) {
+    const contenido = document.querySelector('#platillos .contenido')
+
+    platillos.forEach(platillo => {
+        const row = document.createElement('DIV');
+        row.classList.add('row', 'py-3', 'border-top');
+
+        const nombre = document.createElement('DIV');
+        nombre.classList.add('col-md-4');
+        nombre.textContent = platillo.nombre;
+
+        const precio = document.createElement('DIV');
+        precio.classList.add('col-md-3', 'fw-bold');
+        precio.textContent = `$${platillo.precio}`
+
+        const categoria = document.createElement('DIV');
+        categoria.classList.add('col-md-3')
+        categoria.textContent = categorias[ platillo.categoria ];
+
+        const inputCantidad = document.createElement('INPUT');
+        inputCantidad.type = 'number';
+        inputCantidad.min = 0;
+        inputCantidad.value = 0;
+        inputCantidad.id = `producto-${platillo.id}`;
+        inputCantidad.classList.add('form-control');
+
+        //Funcion que detecta la cantidad y el platillo que se esta agregando
+
+        inputCantidad.onchange = agregarPlatillo(platillo.id);
+
+        const agregar = document.createElement('DIV');
+        agregar.classList.add('col-md-2');
+        agregar.appendChild(inputCantidad);
+
+        row.appendChild(nombre);
+        row.appendChild(precio);
+        row.appendChild(categoria);
+        row.appendChild(agregar);
+
+
+        contenido.appendChild(row);
+    })
+}
+
+function agregarPlatillo(id) {
+    console.log('Desde agregar platillo')
+}
